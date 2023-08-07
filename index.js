685d79f539ef32477951f2bef72d030d47247da4
Emmanuel Valentin <ema_110@outlook.es>
2023-08-07 00:22:32 -0600

    Se movió el código hecho desde cero a esta nueva app

---
+++ index.js
@@ -0,0 +1,22 @@
+import 'normalize.css';
+const toogleButtonElement = document.querySelector('#btn-nav');
+const navElement = document.querySelector('#nav');
+
+const setIsNavVisible = (event) => {
+  console.log(event.target);
+  let isNavVisible = !navElement.classList.contains('d-none');
+  isNavVisible
+    ? (toogleButtonElement.innerHTML =
+        '<img src="./src/assets/icons/menu.svg" alt="Menu icon" />')
+    : (toogleButtonElement.innerHTML =
+        '<img src="./src/assets/icons/close.svg" alt="Close icon" />');
+
+  navElement.classList.toggle('d-none');
+
+  isNavVisible = !isNavVisible;
+};
+
+toogleButtonElement.addEventListener('click', setIsNavVisible);
+
+// Esconde el nav cuando se hace click en algún enlace
+navElement.addEventListener('click', setIsNavVisible);
