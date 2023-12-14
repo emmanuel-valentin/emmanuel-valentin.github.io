7594ab62ce38da6d810967221149f9db410174d5
Emmanuel Valentin <ema_110@outlook.es>
2023-12-13 23:37:13 -0600

    Mensaje provisional

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
-      <h1 className="text-blue-500 text-6xl font-bold">Emmanuel Valentin</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center text-blue-500 dark:text-blue-100 bg-gray-100 dark:bg-slate-900">
+      <h1 className="text-4xl md:text-6xl font-bold">Emmanuel Valentin</h1>
+      <p className="text-lg">
+        Próximamente trabajaré en esta nueva versión. 🤠
+      </p>
     </main>
   );
 }
