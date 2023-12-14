cd7652d35c9db761236c691f245d9d1874e3b0c8
Emmanuel Valentin <ema_110@outlook.es>
2023-12-13 22:04:15 -0600

    Change current font to Roboto

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from 'next/image';
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-green-900 text-6xl">Emmanuel Valentin</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-blue-500 text-6xl font-bold">Emmanuel Valentin</h1>
     </main>
   );
 }
